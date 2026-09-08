f31708b36ad8e3967fdd1c5d85e45ede4ab5817f
Natalia Padilla <natacrack0000@gmail.com>
2026-09-08 15:19:32 -0600

    Fix missing app initialization in public/index.php to resolve 500 error

--- public/index.php
+++ public/index.php
@@ -15,6 +15,8 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 try {
+    /** @var \Illuminate\Foundation\Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
     $app->handleRequest(Request::capture());
 } catch (\Throwable $e) {
     http_response_code(500);
